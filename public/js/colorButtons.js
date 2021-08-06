b8dbf15037c5a1d95937cf3991e39de1f672b36c
Alexandra <sasha.lebedeva30@gmail.com>
2021-08-06 17:48:24 +0300

    color2

--- public/js/colorButtons.js
+++ public/js/colorButtons.js
@@ -1,10 +1,18 @@
 const allColors = document.querySelector('.colors')
-const selectedColor = document.querySelector('.selectedColor')
+const selectedColor = document.querySelector('#primaryColor')
 
 document.addEventListener('click', async (e) => {
   if (e.target.parentNode.classList.contains('colors')) {
-    console.log(e.target);
-    delete selectedColor.classList[0].background
-    selectedColor.classList.add(e.target.classList[0])
+    selectedColor.classList.remove(...selectedColor.classList)
+    selectedColor.classList.add(e.target.classList[0], 'selectedColor')
   }
 })
+
+const selectedColor1 = document.querySelector('#secondaryColor')
+document.addEventListener('click', async (e) => {
+  if (e.target.parentNode.classList.contains('colors')) {
+    selectedColor1.classList.remove(...selectedColor1.classList)
+    selectedColor1.classList.add(e.target.classList[0], 'selectedColor')
+  }
+})
+
